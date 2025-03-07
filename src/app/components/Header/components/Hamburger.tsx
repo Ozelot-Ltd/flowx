d061c66502e0b8dc53f4eec19f977e0037ab91a3
carloozed <carlo@ozelot.ltd>
2025-03-07 14:42:20 +0100

    changes

--- src/app/components/Header/components/Hamburger.tsx
+++ src/app/components/Header/components/Hamburger.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 import styles from './Hamburger.module.css';
 
+import { useScrollStore } from '../../../../../stores/useWindowStore';
+
 type Props = {
   isMenuOpen: boolean;
   setIsMenuOpen: (value: boolean) => void;
 };
 
 export default function Hamburger({ isMenuOpen, setIsMenuOpen }: Props) {
+  const { isScroll } = useScrollStore();
   return (
     <div
-      className={`${styles.hamburger} ${isMenuOpen ? styles.active : ''}`}
+      className={`${styles.hamburger} ${isMenuOpen ? styles.active : ''}
+      ${!isScroll ? styles.hidden : ''}`}
       onClick={() => setIsMenuOpen(!isMenuOpen)}
       aria-expanded={isMenuOpen}
       aria-label="Toggle menu"
