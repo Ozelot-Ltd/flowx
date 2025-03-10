decf436104e8a3fc07884c0e95c28d92c498de88
carloozed <carlo@ozelot.ltd>
2025-03-10 15:34:21 +0100

    changes

--- src/slices/ReferenceSlice/index.tsx
+++ src/slices/ReferenceSlice/index.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import { Content } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 import SectionContainer from '@/app/components/HomeContent/SectionContainer';
 import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 
 import styles from './index.module.css';
+import MainHeadingContainer from '@/app/components/Containers/MainHeadingContainer';
 
 /**
  * Props for `ReferenceSlice`.
@@ -24,16 +25,21 @@ const ReferenceSlice: FC<ReferenceSliceProps> = ({ slice }) => {
         className={styles.container}
       >
         <div className={styles.contentContainer}>
+          {' '}
+          <MainHeadingContainer>
+            <PrismicRichText field={slice.primary.title} />
+          </MainHeadingContainer>
           <div className={styles.groupContainer}>
             {slice.primary.reference_group.map((item, index) => (
               <div key={index}>
-                <PrismicNextLink field={item.reference_link}>
-                  <div className={styles.group}>
+                <div className={styles.group}>
+                  {/* <PrismicRichText field={item.reference_name} /> */}
+                  <PrismicNextLink field={item.reference_link}>
                     <div className={styles.iconContainer}>
                       <PrismicNextImage field={item.reference_image} />
-                    </div>
-                  </div>
-                </PrismicNextLink>
+                    </div>{' '}
+                  </PrismicNextLink>
+                </div>
               </div>
             ))}
           </div>
