2bac60448fb58ca018ec7f0e1dee03e1233d2bd9
carloozed <carlo@ozelot.ltd>
2025-03-18 13:59:04 +0100

    changes

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -16,6 +16,18 @@ export default function Header({ settings }: { settings: SettingsDocument }) {
   const { isScroll } = useScrollStore();
   const { setActiveSection } = useNavigation();
 
+  const scrollToBottom = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+    setActiveSection('contact');
+  };
+
   return (
     <header
       className={`${styles.header} ${!isScroll ? styles.transparent : ''}`}
@@ -31,7 +43,10 @@ export default function Header({ settings }: { settings: SettingsDocument }) {
         </div>
       </div>
       <div className={styles.contactContainer}>
-        <Link href={'mailto:info@flowx.one'}>CONTACT</Link>
+        {/* Replace the mailto link with a normal link that triggers the scroll function */}
+        <a href="#contact" onClick={scrollToBottom}>
+          CONTACT
+        </a>
       </div>
     </header>
   );
