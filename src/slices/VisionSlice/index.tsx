e0e2317c1fb3565ad606391fd026aabdb1cde5cc
carloozed <carlo@ozelot.ltd>
2025-03-20 08:29:44 +0100

    changes

--- src/slices/VisionSlice/index.tsx
+++ src/slices/VisionSlice/index.tsx
@@ -42,7 +42,7 @@ const VisionSlice: FC<VisionSliceProps> = ({ slice }) => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/bBydQkBwxzE?si=KmtaoIhn-JfqiklY&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=bBydQkBwxzE"
+                src="https://www.youtube.com/embed/96FiPnc7osA?si=XZIWJ7LfC50oReH9&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=96FiPnc7osA"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
