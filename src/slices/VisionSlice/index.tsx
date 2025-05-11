4af51b69c191123f6020925467e54ad7d3a609c6
carloozed <carlo@ozelot.ltd>
2025-05-11 12:56:30 +0200

    flowx sucks

--- src/slices/VisionSlice/index.tsx
+++ src/slices/VisionSlice/index.tsx
@@ -36,7 +36,7 @@ const VisionSlice: FC<VisionSliceProps> = ({ slice }) => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/96FiPnc7osA?si=XZIWJ7LfC50oReH9&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=96FiPnc7osA"
+                src="https://www.youtube.com/embed/bpTvgYByvH4?si=LsHVVpg-5RYDvGDj&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=96FiPnc7osA"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
