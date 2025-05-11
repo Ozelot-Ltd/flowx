20a0a4c70c71d33ddd98f44f65ef15b5510c1fce
carloozed <carlo@ozelot.ltd>
2025-05-11 13:04:41 +0200

    finish flowcx

--- src/slices/VisionSlice/index.tsx
+++ src/slices/VisionSlice/index.tsx
@@ -36,7 +36,7 @@ const VisionSlice: FC<VisionSliceProps> = ({ slice }) => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/bpTvgYByvH4?si=LsHVVpg-5RYDvGDj&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=96FiPnc7osA"
+                src="https://www.youtube.com/embed/bpTvgYByvH4?si=LsHVVpg-5RYDvGDj&autoplay=1&mute=1&modestbranding=1&color=white&loop=1&playlist=bpTvgYByvH4"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
